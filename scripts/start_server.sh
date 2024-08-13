9f72bb01b7ede384b6728bde6d0577c9e1092ca3
Lumither Tao <lumither@outlook.com>
2024-08-13 22:32:04 +0800

    feat(global) container and project structure update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,12 +4,17 @@
 /bin/bash ./flush_env.sh
 
 # start frontend
+(
 cd frontend || exit
+bun i
 bun next build
 screen -S nocturne_frontend -d -m bun start
 cd ..
+) &
 
 # start backend
+(
 cd backend || exit
 screen -S nocturne_backend -d -m cargo r -r
 cd ..
+) & wait
