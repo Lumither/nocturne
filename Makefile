20c296424249b666ce72938cd0d510b5a1312ff8
Lumither Tao <lumither@outlook.com>
2024-12-08 22:25:47 -0800

    feat(container) production build

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-ddev:
+ARCH := $(shell uname -m)
+ifeq ($(ARCH), arm64)
+    ARCH := aarch64
+endif
+
+all: release
+
+release:
+	docker compose -f prod.compose.yaml build --build-arg ARCH=$(ARCH)
+
+dev:
 	docker-compose -f dev.compose.yaml up --build
 
-dclean:
-	docker-compose -f dev.compose.yaml rm -fsv
+clean:
+	docker-compose -f prod.compose.yaml -f dev.compose.yaml rm -fsv
 
--- backend/api_server/src/blog/cron/mod.rs
+++ backend/api_server/src/blog/cron/mod.rs
@@ -7,7 +7,7 @@ pub mod check_update;
 pub fn get_tasks(db_connection: Pool<Postgres>) -> Vec<Box<dyn Task>> {
     let db_conn_clone = db_connection.clone();
     vec![
-        BasicTask::new(move || check_update::task(&db_conn_clone), "0 * * * * *")
+        BasicTask::new(move || check_update::task(&db_conn_clone), "0/15 * * * * *")
             .unwrap()
             .to_task(),
     ]
